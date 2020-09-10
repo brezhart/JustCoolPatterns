4b109956a84065518b729b41c0ce2aeb4fca7451
brezhart <47702438+brezhart@users.noreply.github.com>
2020-09-10 10:49:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
-
-
-
-
-
-
-
-
-
-
 (async () => {
-
-
-
-
-
     function f(y,x) {
         return  Math.tan(x^y)> 0.5;
     }
-
-
     const fs = require('fs');
     const { createCanvas} = require('canvas');
-
-
-    let gridSize = (1<<8);
-
-    let p = 1;
-
-
+    
+    let gridSize = (1<<8); // Size of grid
+    let p = 1; // Pixel size
+    
+    
     const canvas = createCanvas(gridSize*p, gridSize*p);
     const ctx = canvas.getContext('2d');
+    
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(0,0,gridSize*p,gridSize*p);
+    ctx.fillRect(0,0,gridSize*p,gridSize*p); // White background
     ctx.fillStyle = "#000000";
+    
     for (let i = 0; i < gridSize; i++){
         for (let g = 0; g < gridSize; g++){
             if (f(i,g)){
-                ctx.fillRect(i*p,g*p,p,p );
+                ctx.fillRect(i*p,g*p,p,p);
             }
         }
     }
